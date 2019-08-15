cb6cd122f43f99ec497ad83a3b83bea00dfa3b42
carlosbunn <carlos.bunn@gmail.com>
2019-08-15 07:05:00 +0000

    fixed script for terraform

--- resources/terraform-path.sh
+++ resources/terraform-path.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -q terraform ~/.bashrc || echo "export PATH=$PATH:/opt/terraform" >> ~/.bashrc/ && . ~/.bashrc
+grep -q terraform ~/.bashrc || echo "export PATH=$PATH:/opt/terraform" >> ~/.bashrc && . ~/.bashrc
