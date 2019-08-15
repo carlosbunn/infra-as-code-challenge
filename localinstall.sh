cf7b569d331b689ac1b71fc2e3cf82f913af982d
carlosbunn <carlos.bunn@gmail.com>
2019-08-15 02:12:41 +0000

    Fixed terraform installation

--- localinstall.sh
+++ localinstall.sh
@@ -7,7 +7,7 @@ cd /opt/terraform
 wget https://releases.hashicorp.com/terraform/0.12.6/terraform_0.12.6_linux_amd64.zip
 unzip terraform_0.12.6_linux_amd64.zip
 rm terraform_0.12.6_linux_amd64.zip
-chmod 777 /opt/terraform/terraform
+chmod 755 /opt/terraform/terraform
 echo 'export PATH=$PATH:/opt/terraform/' >> ~/.bashrc
 cd ~
 . ~/.bashrc
